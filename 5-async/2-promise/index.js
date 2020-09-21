b174617a928b4e36796c377b354513edddb8757d
syq <1035394794@qq.com>
2020-09-21 22:06:47 +0800

    [syq] complete async promise

--- 5-async/2-promise/index.js
+++ 5-async/2-promise/index.js
@@ -3,7 +3,15 @@ function fetchData(url) {
     const xhr = new XMLHttpRequest();
     // <-- start
     // TODO 22: 通过Promise实现异步请求
-
+    xhr.onreadystatechange = function onchange() {
+      if (xhr.readyState === 4 && (xhr.status >= 200 || xhr.status < 300)) {
+        resolve(xhr.responseText);
+      } else if (xhr.readyState === 4 && (xhr.status < 200 || xhr.status >= 300)) {
+        reject(xhr.error);
+      }
+    };
+    xhr.open('get', url, true);
+    xhr.send();
     // end -->
   });
 }
