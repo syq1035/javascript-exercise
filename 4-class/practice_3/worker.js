d1a182b92f2f71454a60ded2a91ab7394f575774
syq <1035394794@qq.com>
2020-09-21 21:15:10 +0800

    [syq] complete class practice3

--- 4-class/practice_3/worker.js
+++ 4-class/practice_3/worker.js
@@ -1,1 +1,8 @@
-// TODO 14: 在这里写实现代码
+import Person from './person';
+
+export default class Worker extends Person {
+  /* eslint class-methods-use-this: ["error", { "exceptMethods": ["introduce"] }] */
+  introduce() {
+    return `I am a Worker. I have a job.`;
+  }
+}
