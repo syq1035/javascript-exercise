6fcf950e4f9ca0c43665649b884c61f22ef2f310
syq <1035394794@qq.com>
2020-09-21 22:03:09 +0800

    [syq] complete async xhr

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,15 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
-
+  xhr.onreadystatechange = function onchange() {
+    if (xhr.readyState === 4 && (xhr.status >= 200 || xhr.status < 300)) {
+      successCallback(xhr.responseText);
+    } else if (xhr.readyState === 4 && (xhr.status < 200 || xhr.status >= 300)) {
+      errorCallback(xhr.error);
+    }
+  };
+  xhr.open('get', url, true);
+  xhr.send();
   // end -->
 }
 
