80a91dbb6e5274b5032f6abddedacaf4c595e25b
syq <1035394794@qq.com>
2020-09-21 19:56:43 +0800

    [syq] complete object assign

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,10 @@
 export default function addSerialNumber(source) {
-  // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  Object.assign(source.properties, {
+    color: 'green',
+    status: 'processed'
+  });
+  Object.assign(source, {
+    serialNumber: '12345'
+  });
+  return source;
 }
